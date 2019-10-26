1e391bd4304cebe496a6ae740b241585471ee5f8
Park Jongho <kim010c@gmail.com>
2019-10-26 22:51:14 +0900

    setState

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
 import React from "react";
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "haryy",
-    image:
-      "https://postfiles.pstatic.net/20151001_268/iezynvjwr_1443688686125tVhdb_JPEG/%C7%D8%B8%AE%C6%F7%C5%CD1.jpg?type=w3",
-    rating: 2.2 / 5
-  },
-  {
-    id: 2,
-    name: "ron",
-    image:
-      "https://postfiles.pstatic.net/20151001_268/iezynvjwr_1443688686125tVhdb_JPEG/%C7%D8%B8%AE%C6%F7%C5%CD1.jpg?type=w3",
-    rating: 5 / 5
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count + -1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>number : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-function Movie({ name, picture, rating }) {
-  return (
-    <div>
-      <h1>I hate {name}</h1>;
-      <img src={picture} alt={name} />
-      <h4>{rating}</h4>
-    </div>
-  );
 }
-
-Movie.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Movie
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-}
-
 export default App;
