f604f772dae9f12019ba406bc9b6b4ce37290ed9
Park Jongho <kim010c@gmail.com>
2019-10-27 16:01:13 +0900

    Updates

---
+++ precache-manifest.b13631d1a53c00e4eb0278716fc791de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1b0804db3c98dd74f44d536843c75f",
+    "url": "/kim010c/movie_app/Movie/index.html"
+  },
+  {
+    "revision": "2386f793aae025db26dd",
+    "url": "/kim010c/movie_app/Movie/static/css/main.888d1aad.chunk.css"
+  },
+  {
+    "revision": "54662ea1af6317c46be7",
+    "url": "/kim010c/movie_app/Movie/static/js/2.d2a361f7.chunk.js"
+  },
+  {
+    "revision": "2386f793aae025db26dd",
+    "url": "/kim010c/movie_app/Movie/static/js/main.bd7bd7e1.chunk.js"
+  },
+  {
+    "revision": "8c99b67596bbdd05fb08",
+    "url": "/kim010c/movie_app/Movie/static/js/runtime-main.b048ae6f.js"
+  }
+]);
